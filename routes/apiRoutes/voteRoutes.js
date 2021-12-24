b2b4bcc1cd464045f91b0d33261692ee1101f77b
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-12-24 00:54:46 -0600

    updated votes route

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -5,12 +5,13 @@ const inputCheck = require('../../utils/inputCheck');
 
 router.get('/votes', (rew, res)=>{
 
-    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id)
-    AS count 
-    FROM votes 
+    const sql = `SELECT candidates.*, parties.name AS party_name, 
+    COUNT(candidate_id)
+    AS count FROM votes 
     LEFT JOIN candidates ON votes.candidate_id = candidates.id
     LEFT JOIN parties ON candidates.party_id = parties.id
-    GROUP BY candidate_id ORDER BY count DESC`;
+    GROUP BY candidate_id 
+    ORDER BY count DESC`;
 
     db.query(sql, (err, rows)=>{
         if(err){
