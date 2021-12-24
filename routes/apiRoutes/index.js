2207a9e609197d71db604faf3b646b3b485ebba7
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-12-23 22:22:16 -0600

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// router.use(express.urlencoded({ extended: false }));
+// router.use(express.json());
+
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
